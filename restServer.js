938cbb33575e8adc13d5cfb97ac7662a6ddc65b6
steAz <urokcomps@gmail.com>
2021-01-31 18:58:57 +0100

    Added react app for frontend of the app and created get request for first page

--- restServer.js
+++ restServer.js
@@ -1,1 +1,13 @@
-console.log('Created rest server')
+const express = require('express');
+const app = express();
+
+
+app.get('/', function(req, res) {
+  res.send('Typical get response');
+})
+
+app.listen(80, function() {
+  console.log('Now app listening on 80')
+});
+
+
